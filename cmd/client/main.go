2570399dda02e96435892fbc3a113bab31b0210a
finlleyl <ilya_chibugaev@mail.ru>
2025-01-15 15:18:04 +0300

    refactoring client using resty

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"net/http"
+	"github.com/go-resty/resty/v2"
 	"net/url"
 	"os"
 	"strings"
@@ -25,25 +24,17 @@ func main() {
 	long = strings.TrimSuffix(long, "\n")
 	data.Set("url", long)
 
-	client := &http.Client{}
+	client := resty.New()
+	response, err := client.R().
+		SetHeader("Content-Type", "application/x-www-form-urlencoded").
+		SetBody(data.Encode()).
+		Post(endpoint)
 
-	request, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(data.Encode()))
 	if err != nil {
 		panic(err)
 	}
 
-	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	response, err := client.Do(request)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("Response Status Code: ", response.StatusCode())
+	fmt.Println("Response Body: ", response.String())
 
-	fmt.Println("Статус-код ", response.Status)
-	defer response.Body.Close()
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(body))
 }
