c3ac15a10d311afb57eae616900f1d37e3dac43e
finlleyl <ilya_chibugaev@mail.ru>
2025-01-30 22:51:51 +0400

    repaired middleware

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -34,28 +34,44 @@ func main() {
 
 func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// По умолчанию ответ пишем в ow = w
 		ow := w
 
+		// Определяем, хочет ли клиент gzip
 		acceptEncoding := r.Header.Get("Accept-Encoding")
 		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+
+		// Если да, оборачиваем w в gzip.Writer
 		if supportsGzip {
+			w.Header().Set("Content-Encoding", "gzip")
+			w.Header().Add("Vary", "Accept-Encoding")
+
 			cw := newCompressWriter(w)
 			ow = cw
 			defer cw.Close()
 		}
 
+		// А теперь на уровне запроса (входящих данных) проверяем,
+		// не пришел ли он к нам gzipped
 		contentEncoding := r.Header.Get("Content-Encoding")
 		sendsGzip := strings.Contains(contentEncoding, "gzip")
-		if sendsGzip && (strings.Contains(r.Header.Get("Content-Type"), "application/json") || strings.Contains(r.Header.Get("Content-Type"), "text/html")) {
+
+		// Если клиент отправил gzip и это данные типа JSON или HTML,
+		// мы распаковываем Body (необязательная логика, зависит от API).
+		if sendsGzip && (strings.Contains(r.Header.Get("Content-Type"), "application/json") ||
+			strings.Contains(r.Header.Get("Content-Type"), "text/html")) {
 			cr, err := newCompressReader(r.Body)
 			if err != nil {
+				// Ошибка при создании gzip.Reader
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
+			// Переопределяем r.Body
 			r.Body = cr
 			defer cr.Close()
 		}
 
+		// И вызываем основной хендлер
 		h.ServeHTTP(ow, r)
 	}
 }
