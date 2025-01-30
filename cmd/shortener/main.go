4bafcb0f4307e82109a1aa513d21e300ddeec876
finlleyl <ilya_chibugaev@mail.ru>
2025-01-30 20:10:31 +0400

    added gzip

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/finlleyl/shorty/internal/logger"
 	"github.com/go-chi/chi/v5"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -28,5 +29,40 @@ func main() {
 	logger.Sugar.Infow("Server started", "address", cfg.A.Address)
 	if err := http.ListenAndServe(cfg.A.Address, r); err != nil {
 		logger.Sugar.Fatalw("Server failed", "error", err)
+	}
+}
+
+func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ow := w
+
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		if supportsGzip {
+			cw := newCompressWriter(w)
+
+			ow = cw
+
+			defer cw.Close()
+		}
+
+		contentType := r.Header.Get("Content-Type")
+		goodToGzip := strings.Contains(contentType, "text/plain") || strings.Contains(contentType, "text/html")
+		if goodToGzip {
+			contentEncoding := r.Header.Get("Content-Encoding")
+			sendsGzip := strings.Contains(contentEncoding, "gzip")
+			if sendsGzip {
+				cr, err := newCompressReader(r.Body)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				r.Body = cr
+
+				defer cr.Close()
+			}
+		}
+		h.ServeHTTP(ow, r)
 	}
 }
